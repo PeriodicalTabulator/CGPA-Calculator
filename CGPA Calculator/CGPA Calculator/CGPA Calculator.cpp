5338d0dd9fc7b7fee88b9541c021a4b588a3a7c2
PeriodicalTabulator <146066642+PeriodicalTabulator@users.noreply.github.com>
2024-10-28 19:56:06 +0100

    Add files via upload

--- CGPA Calculator/CGPA Calculator/CGPA Calculator.cpp
+++ CGPA Calculator/CGPA Calculator/CGPA Calculator.cpp
@@ -1,28 +1,38 @@
-
 #include <iostream>
 #include <vector>
+
 using namespace std;
 
-int main()
-{
-	int numSubject;
-	int numSemester;
-	cout << "How many semesters you had? ";
-	cin >> numSemester;
-	cout << "How many subjects you had? ";
-	cin >> numSubject;
-	vector<float> credits(numSubject), grades(numSubject);
-	float totalCredits = 0;
-	float weightedSum = 0;
-	for (int i = 0; i < numSubject; i++) {
-		cout << "Enter credit for subject " << i + 1 << ": ";
-		cin >> credits[i];
-		cout << "Enter grade for subject " << i + 1 << ": ";
-		cin >> grades[i];
-		totalCredits += credits[i];
-		weightedSum += credits[i] * grades[i];
-	}
-	float CGPA = weightedSum / totalCredits;
-	cout << "Your CGPA is: " << CGPA << endl;
-	return 0;
+int main() {
+    int numSemester;
+    int numSubject;
+
+    cout << "How many semesters you had? ";
+    cin >> numSemester;
+
+    for (int i = 0; i < numSemester; i++) {
+        cout << "For semester " << i + 1 << ":\n";
+        cout << "How many subjects you had? ";
+        cin >> numSubject;
+
+        vector<float> credits(numSubject), grades(numSubject);
+        float totalCredits = 0;
+        float weightedSum = 0;
+
+        for (int j = 0; j < numSubject; j++) {
+            cout << "Enter credit for subject " << j + 1 << ": ";
+            cin >> credits[j];
+
+            cout << "Enter grade for subject " << j + 1 << ": ";
+            cin >> grades[j];
+
+            totalCredits += credits[j];
+            weightedSum += credits[j] * grades[j];
+        }
+
+        float CGPA = weightedSum / totalCredits;
+        cout << "Your CGPA for semester " << i + 1 << " is: " << CGPA << endl;
+    }
+
+    return 0;
 }
